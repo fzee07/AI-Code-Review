5c1256aada7913f7a7757f4840f9c1041529f7c2
fzee07 <hassanfaizan1994@gmail.com>
2026-05-24 23:39:22 +0530

    updating for vercel

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async (): Promise<void> => {
+  if (isConnected) return;
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI as string);
+    isConnected = true;
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error: any) {
     console.error(`MongoDB Connection Error: ${error.message}`);
